20100c3f3aaf7d7f04000969ac062459337d3fff
poi-vrc <77053052+poi-vrc@users.noreply.github.com>
2022-10-08 12:05:33 +0800

    Add trim

--- src/main.ts
+++ src/main.ts
@@ -15,18 +15,18 @@ async function run(): Promise<void> {
     let wsPath = <string>process.env.GITHUB_WORKSPACE;
     core.info("Workspace path: " + wsPath);
 
+    core.info("Version text path: " + versionTextFilePath);
+    core.info("Package JSON path: " + packageJsonFilePath);
+
     let versionTextStr = <string><unknown>fs.readFileSync(path.join(wsPath, versionTextFilePath));
     let packageJsonStr = <string><unknown>fs.readFileSync(path.join(wsPath, packageJsonFilePath));
-
-    core.info("Version text path: " + versionTextStr);
-    core.info("Package JSON path: " + packageJsonStr);
 
     let packageJson = JSON.parse(packageJsonStr);
 
     core.info("Version text string: " + versionTextStr);
     core.info("Package JSON string: " + packageJson['version']);
 
-    if (versionTextStr !== packageJson['version']) {
+    if (versionTextStr.trim() !== packageJson['version'].trim()) {
       core.setFailed('Version text does not match with package JSON version string.');
     }
   } catch (error) {
